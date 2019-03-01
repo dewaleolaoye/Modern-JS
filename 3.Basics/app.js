3b1c220b9009e30032418aa74c0628e515ef717e
dewaleolaoye <adewaleolaoye21@gamil.com>
2019-03-01 23:45:38 +0100

    temperature conversion

--- 3.Basics/app.js
+++
@@ -1,4 +0,0 @@
-let city = "Lagos";
-let country = "Nigeria";
-let location = city + ", " + country;
-console.log(location);
---
+++ 3.Basics/temp-converter.js
@@ -0,0 +1,12 @@
+// convert fahrenheit to celcius formula
+// Tc = (Tf-32) * 5/9
+//
+let fahrenheit = 32;
+let celcius = (fahrenheit - 32) * (5 / 9);
+
+console.log(celcius + "C");
+
+// convert fahrenheit to kelvin
+
+let kelvin = (fahrenheit + 459.67) * (5 / 9);
+console.log(kelvin + "K");
