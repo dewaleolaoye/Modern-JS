5fc0b266b298efb69d93a92508701ac9fa28ebb3
dewaleolaoye <adewaleolaoye21@gamil.com>
2019-03-02 09:18:21 +0100

    if statement

--- 3.Basics/booleans.js
+++ 3.Basics/booleans.js
@@ -6,8 +6,18 @@
 // <= less than and equal to
 
 // challenge
-let age = 26;
-let isChild = age <= 7;
-let isSenior = age >= 65;
-console.log(isChild);
-console.log(isSenior);
+// let age = 26;
+// let isChild = age <= 7;
+// let isSenior = age >= 65;
+// console.log(isChild);
+// console.log(isSenior);
+let age = 65;
+if (age <= 7) {
+  console.log("Child Pricing is $50");
+} else {
+  console.log("Child is not getting any discount");
+}
+
+if (age >= 65) {
+  console.log("Old Man, You will get discount of £10");
+}
