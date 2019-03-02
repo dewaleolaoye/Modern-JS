d9bdaacf89e3a6157c86bf645973b0400e2d7936
dewaleolaoye <adewaleolaoye21@gamil.com>
2019-03-02 22:52:59 +0100

    Objects basics

---
+++ 5.Objects/object-101.js
@@ -0,0 +1,22 @@
+// let myBook = {
+//   title: "Maximize Destiny",
+//   author: "David Oyedepo",
+//   pageCount: 220
+// };
+// // console.log(myBook.title + " by " + myBook.author);
+// console.log(`${myBook.title} by ${myBook.author} is ${myBook.pageCount} pages`);
+
+// challenge
+let human = {
+  age: 26,
+  name: "Wale",
+  location: "Lagos"
+};
+console.log(
+  `${human.name} is ${human.age} years old and lives in ${human.location}`
+);
+
+human.name = "Adewale";
+console.log(
+  `${human.name} is ${human.age} years old and lives in ${human.location}`
+);
