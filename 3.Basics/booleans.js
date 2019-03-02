2489c60c010c6b0e596667ae90d7bf6968476d24
dewaleolaoye <adewaleolaoye21@gamil.com>
2019-03-02 08:48:53 +0100

    booleans

---
+++ 3.Basics/booleans.js
@@ -0,0 +1,13 @@
+// === equality operator
+// !== not equal operator
+// < less than
+// > greater than
+// >= greater and equal to
+// <= less than and equal to
+
+// challenge
+let age = 26;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+console.log(isChild);
+console.log(isSenior);
