3f46c284d5c12099f491c0b20ebbd51bf533de28
dewaleolaoye <adewaleolaoye21@gamil.com>
2019-03-10 00:11:20 +0100

    Arrays

---
+++ 6.Arrays/notes.js
@@ -0,0 +1,4 @@
+const note = ["Note 1", "Note 2", "Note 3"];
+console.log(note[2]);
+console.log(note.length);
+console.log(note[note.length - 1]);
---
+++ 6.Arrays/todo.js
@@ -0,0 +1,12 @@
+const todos = [
+  "Read the Bible",
+  "Pray",
+  "Study JS",
+  "Read about IoT",
+  "Andela Test"
+];
+
+console.log(`You have ${todos.length} todos`);
+
+console.log("Todo:", todos[todos.length - 1]);
+console.log("Todo:", todos[todos.length - 2]);
