b8b08a9f7bf4c0798626cf14562255f4ea60412e
dewaleolaoye <adewaleolaoye21@gamil.com>
2019-03-09 10:07:15 +0100

    object method

---
+++ 5.Objects/object-method.js
@@ -0,0 +1,21 @@
+// A Method is an object property whose value is a function
+let restaurant = {
+  name: "ASB",
+  guestCapacity: 75,
+  guestCount: 0,
+  checkAvailability: function(partySize) {
+    let seatLeft = this.guestCapacity - this.guestCount;
+    return partySize <= seatLeft;
+  },
+  seatParty: function(partySize) {
+    this.guestCount = this.guestCount + partySize;
+  },
+  removeParty: function(partySize) {
+    this.guestCount = this.guestCount - partySize;
+  }
+};
+
+restaurant.seatParty(72);
+console.log(restaurant.checkAvailability(4));
+restaurant.removeParty(5);
+console.log(restaurant.checkAvailability(4));
